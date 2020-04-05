ebde725cdba156dccfa5d5e986706a45680a2d9b
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-04-04 20:26:05 -0400

    Updating server side

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const gameController = require('./controllers/game');
+const usersController = require('./controllers/users');
+
 
 
 const app = express();
@@ -7,11 +11,21 @@ const port = 3000;
 
 app
     .use(express.json())
-    .use(express.urlencoded({ extended: true}))
-    .use(express.static(__dirname+'/../client/dist'))
+    .use(express.urlencoded({ extended: true }))
+    .use(express.static( __dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('This class is awesome!') )
     .use('/game', gameController)
+    .use('/users', usersController)
 
-    .use((req, res)=> res.sendFile(__dirname+'/../client/dist/index.html'))
+    .use((req, res) => {
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
+        console.log(homePath);
+        res.sendFile(homePath)
+    })
+    .use( (err, req, res, next ) => {
+        console.error(err);
+        const errorCode = err.code || 500;
+        res.status(errorCode).send({ message: err.message });
+    } )
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
